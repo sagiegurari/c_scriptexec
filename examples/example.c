202b95a69408e91d71d7afd757b18a67c41f2ed9
sagie gur ari <sagiegurari@gmail.com>
2020-11-07 07:25:48 +0000

    Format examples during build

--- examples/example.c
+++ examples/example.c
@@ -2,18 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
   // invoke the script with default options
   struct ScriptExecResult result = scriptexec_run("echo 1\necho 2\necho 3\necho 4");
+
   printf("Code: %d\nOutput:\n%s\nError:\n%s\n", result.code, result.out, result.err);
 
   // invoke the script with custom options
   struct ScriptExecOptions options = scriptexec_create_options();
-  options.runner = "bash"; // default is 'sh'
+  options.runner            = "bash";     // default is 'sh'
   options.working_directory = "./target"; // default to current working directory
-  options.exit_on_error = true; // default true, will break script execution on any error
-  options.print_commands = true; // default false, if true will print every command before invocation
+  options.exit_on_error     = true;       // default true, will break script execution on any error
+  options.print_commands    = true;       // default false, if true will print every command before invocation
 
   result = scriptexec_run_with_options("echo 1\necho 2\necho 3\necho 4", options);
   printf("Code: %d\nOutput:\n%s\nError:\n%s\n", result.code, result.out, result.err);
